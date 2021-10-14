7645154f332bdb94778fcd7bbbaa604d01291898
Santa Maria Shithil <53658987+Santa-Maria-Shithil@users.noreply.github.com>
2021-10-14 16:44:43 +0600

    Update Measurements.java

--- core/src/main/java/site/ycsb/measurements/Measurements.java
+++ core/src/main/java/site/ycsb/measurements/Measurements.java
@@ -181,8 +181,8 @@ public class Measurements {
    * value.
    */
   public void measure(String operation, int latency) {
-   // if (operation == "READ")
-    System.err.println(operation + latency);
+    if (operation == "READ")
+      System.err.println(operation + latency);
     if (measurementInterval == 1) {
       return;
     }
